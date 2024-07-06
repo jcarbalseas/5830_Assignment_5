0bf46be4ff754cfff19ef4a447b955ff4a15c8f1
jcarbalseas <49366476+jcarbalseas@users.noreply.github.com>
2024-07-06 10:27:07 -0700

    Update sign.py

--- sign.py
+++ sign.py
@@ -22,16 +22,14 @@ from eth_account.messages import encode_defunct
 
 def sign(m):
     w3 = Web3()
-
-# Create an Ethereum account
+    
+    # Create an Ethereum account
     account = w3.eth.account.create()
     eth_address = account.address
-    private_key = account.privateKey
+    private_key = account.key  # Correct attribute for the private key
 
-# Encode the message
     message = encode_defunct(text=m)
-
-# Sign the message
+\
     signed_message = w3.eth.account.sign_message(message, private_key)
 
     assert isinstance(signed_message, eth_account.datastructures.SignedMessage)
